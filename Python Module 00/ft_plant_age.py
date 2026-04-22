6aff508cb8119b60e68acc9566eb2c3f3d0cd746
antmanue <antmanue@student.42porto.com>
2026-04-22 01:15:22 +0100

    fix:funtion greater to greater or equal

--- Python Module 00/ft_plant_age.py
+++ Python Module 00/ft_plant_age.py
@@ -2,5 +2,5 @@ def ft_plant_age():
     age_days = int(input("Enter plant age in days:"))
     if age_days < 60:
         print("Plant needs more time to grow.")
-    if (age_days > 60):
+    if (age_days >= 60):
         print("Plant is ready to harvest!")
