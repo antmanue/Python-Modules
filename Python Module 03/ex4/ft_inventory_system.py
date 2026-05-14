878834ab0179ced887b0a8729a20898ddc2273f8
antmanue <antmanue@student.42porto.com>
2026-05-14 16:32:42 +0100

    M03 ex04 almost

--- Python Module 03/ex4/ft_inventory_system.py
+++ Python Module 03/ex4/ft_inventory_system.py
@@ -5,18 +5,49 @@ def dict_build() -> None:
 
     for i in range(1, len(sys.argv)): 
         param = sys.argv[i]
-        try
+        try:
             if ':' not in param:
                 raise ValueError("syntax")
             item, str_quantity = param.split(':')
+            quantity = int(str_quantity)
             if item in inventory:
                 print(f"Redundant item '{item}' discarding")
                 continue
+            inventory[item] = quantity
+
+
         except ValueError as e:
             if str(e) == "syntax":
                 print(f"Error invalid parameter '{param}'")
-            else
+            else:
                 print(f"Quantity error for '{item}: {e}'")
+    return inventory
 
-        quantity = int(str_quantity)
-        inventory[item] = "quantity"
+def main():
+    print(f"=== Inventory System Analysis ===")
+    my_inventory = dict_build()
+    total_qty = sum(my_inventory.values())
+    most_item = ""
+    least_item = ""
+    max_qty = -1
+    min_qty = -1
+    print(f"Got inventory: {my_inventory}")
+    print(f"Item list: {list(my_inventory.keys())}")
+    print(f"Total quantity of the {len(my_inventory)} items: {total_qty}")
+    for item, quantity in my_inventory.items():
+        percent_qty = (quantity*100)/total_qty
+        print(f"Item {item} represents {round(percent_qty, 1)}%")
+        if max_qty == -1 or quantity > max_qty:
+            max_qty = quantity
+            most_item = item
+        if min_qty == -1 or quantity < min_qty:
+            min_qty = quantity
+            least_item = item
+    print(f"Item most abundant {most_item} with quantity: {max_qty}")
+    print(f"Item least abundant {least_item} with quantity: {min_qty}")
+    my_inventory.update:({'magic_item: 1'})
+    print(f"Updated inventory: {my_inventory}")
+
+
+if __name__ == "__main__":
+    main()
