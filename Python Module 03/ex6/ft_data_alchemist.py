2d1547c8f6f59b4a6a441be15b001b04283f3041
antmanue <antmanue@student.42porto.com>
2026-05-15 14:57:04 +0100

    M03 Done

---
+++ Python Module 03/ex6/ft_data_alchemist.py
@@ -0,0 +1,23 @@
+import random
+def main() -> None:
+    players = ['Alice', 'bob', 'Charlie', 'dylan', 'Emma', 'Gregory', 'john', 'kevin', 'Liam']
+    p_all_capitalized = [item.capitalize() for item in players]
+    p_capt_og = [item for item in players if item[0].isupper() == True]
+
+    score_dict = {player: random.randint(0, 1000) for player in p_all_capitalized}
+
+    score_avg = round((sum(score_dict.values()) / len(score_dict.keys())), 2)
+
+    high_score_dict = {player: score for player, score in score_dict.items() if score > score_avg}
+    print("=== Game Data Alchemist ===")
+    print()
+    print(f"Initial list of players: {players}")
+    print(f"New list with all names capitalized: {p_all_capitalized}")
+    print(f"New list of capitalized names only: {p_capt_og}")
+    print()
+    print(f"Score dict: {score_dict}")
+    print(f"Score average is {score_avg}")
+    print(f"High scores: {high_score_dict}")
+
+if __name__ == "__main__":
+    main()
