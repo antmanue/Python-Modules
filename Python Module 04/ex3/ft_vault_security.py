3bd85a3576255ee1dd37a86b31ee4d1dd2f7b1c3
antmanue <antmanue@student.42porto.com>
2026-05-19 02:09:46 +0100

    M04 done

---
+++ Python Module 04/ex3/ft_vault_security.py
@@ -0,0 +1,31 @@
+def secure_archive(file_name: str, action: str = "",text_to_save: str = "") -> tuple[bool, str]:
+    try:
+        if action == "r":
+            with open(file_name, "r") as file:
+                content = file.read()
+            return (True, content)
+        
+        elif action == "w":
+            with open(file_name, "w") as file:
+                file.write(text_to_save)
+            return (True, "Content successfully written to file")
+        
+        else:
+            return (False, "Invalid action. Use 'r' for read or 'w' for write.")
+
+    except Exception as err:
+        return (False, f"{err}")
+    
+def main() -> None:
+    print("=== Cyber Archives Security ===")
+    print("\nUsing 'secure_archive' to read from nonexistent file:")
+    print(secure_archive("/not/existing/file", "r"))
+    print("\nUsing 'secure_archive' to read from inaccessible file:")
+    print(secure_archive("/home/antarva/Documents/42_Porto/Common_Core/Milestone_02/Python-Modules/Python Module 04/ex3/denied.txt", "r"))
+    print("\nUsing 'secure_archive' to read from regular file:")
+    print(secure_archive("/home/antarva/Documents/42_Porto/Common_Core/Milestone_02/Python-Modules/Python Module 04/ex3/ancient_fragment.txt", "r"))
+    print("\nUsing 'secure_archive' to write previous content to a new file:")
+    print(secure_archive("/home/antarva/Documents/42_Porto/Common_Core/Milestone_02/Python-Modules/Python Module 04/ex3/new_vault_file.txt", "w", text_to_save = "Novo teste!"))
+
+if __name__ == "__main__":
+    main()
