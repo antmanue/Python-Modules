199e84d532b204825ce4a86718e2580cc2c2b9a1
antmanue <antmanue@student.42porto.com>
2026-05-26 03:41:52 +0100

    M05 ex0 done

--- Python Module 05/ex0/data_processor.py
+++ Python Module 05/ex0/data_processor.py
@@ -63,7 +63,31 @@ class StringProcessor(DataProcessor):
             self.struct_list.append((self.counter, item))
             self.counter = self.counter + 1
 
+class LogProcessor(DataProcessor):
+    def validate(self, data: typing.Any) -> bool:
+        if isinstance(data, dict) and "log_level" in data and "log_message" in data:
+            return True
+        elif data != ([]) and isinstance(data, list):
+            for item in data:
+                if not isinstance(item, dict) or "log_level" not in item or "log_message" not in item:
+                    return False
+                
+            return True
+        return False
+
+    def ingest(self, data: typing.Any) -> None:
+        if not self.validate(data):
+            raise ValueError ("Improper log data")
+        items =  data if isinstance(data, list) else [data]
+        for item in items:
+            converted = f"{item['log_level']}: {item['log_message']}"
+            self.struct_list.append((self.counter, converted))
+            self.counter = self.counter + 1
+
+
 def main() -> None:
+    print("=== Code Nexus - Data Processor ===")
+    print()
     print("Testing Numeric Processor...")
     numeric_process = NumericProcessor()
 
@@ -71,10 +95,47 @@ def main() -> None:
 
     print(f"Trying to validate input 'Hello': {numeric_process.validate('Hello')} ")
 
+    print(f"Test invalid ingestion of string 'foo' without prior validation:")
     try:
+        numeric_process.ingest("foo")
 
-    except:
+    except ValueError as err:
+        print(f"Got exception: {err}")
+    
+    print("Processing data: [1, 2, 3, 4, 5]")
+    numeric_process.ingest([1, 2, 3, 4, 5])
+    print("Extracting 3 values...")
+    for i in range(3):
+        num_id, num_val = numeric_process.output()
+        print(f"Numeric value {num_id}: {num_val}")
 
+    print()
+    print("Testing Text Processor...")
+    text_process = StringProcessor()
+
+    print(f"Trying to validate input '42': {text_process.validate(42)}")
+    
+    print("Processing data: ['Hello', 'Nexus', 'World']")
+    text_process.ingest(['Hello', 'Nexus', 'World'])
+    print("Extracting 1 value...")
+    tex_id, tex_val = text_process.output()
+    print(f"Text value {tex_id}: {tex_val}")
+    print()
+    print("Testing Log Processor...")
+    log_process = LogProcessor()
+    print(f"Trying to validate input 'Hello': {log_process.validate('Hello')} ")
+    
+    log_data = [
+        {'log_level': 'NOTICE', 'log_message': 'Connection to server'},
+        {'log_level': 'ERROR', 'log_message': 'Unauthorized access!!'}
+    ]
+    print(f"Processing data: {log_data}")
+    log_process.ingest(log_data)
+
+    print("Extracting 2 Values...")
+    for i in range(2):
+        log_id, log_val = log_process.output()
+        print(f"Log entry {log_id}: {log_val}")
 
 if __name__ == "__main__":
     main()
